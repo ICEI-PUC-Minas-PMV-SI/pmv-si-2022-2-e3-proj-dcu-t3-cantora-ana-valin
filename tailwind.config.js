767403e41aad7b60ed19bcd54591cf6533d3002c
amandaclinhares <amandachrislinhares@gmail.com>
2022-10-10 16:46:22 -0300

    Add configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,5 +16,10 @@ module.exports = {
     },
   },
 
-  plugins: [require('tw-elements/dist/plugin')],
+  plugins: [require('tw-elements/dist/plugin'),
+  require('tailwind-scrollbar')],
+
+  variants: {
+    scrollbar: ['rounded']
+}
 }
